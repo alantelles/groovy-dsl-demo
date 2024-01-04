f6ac9aa9cb9d920b14b6e1875b8f6394af2c4eba
Alan Gomes <alan.gomes@zgsolucoes.com.br>
2024-01-04 10:17:02 -0300

    refactor: altera playlist

--- playlists/programa_da_noite.groovy
+++ playlists/programa_da_noite.groovy
@@ -1,8 +1,9 @@
 playlist('Songs for night') {
-    music 'Torn' from 'Natalie Imbruglia'
+    music 'Águas de Março' from 'Tom Jobim'
     music 'You Need A Hero' from 'Pages'
-    music 'You Earned It' from 'The Weeknd'
     music 'The Hills' from 'The Weeknd'
-    announce 'Banco Itaú' saying 'Muito crédito pra vc, sqn'
-    music 'Qualquer Uma' from 'Natalie Imbruglia'
+    announce 'Banco Itaú' saying 'Muito crédito pra você, ou não.'
+    music 'Triste' from 'Tom Jobim'
+    music 'Best Part' from 'H.E.R'
+    announce 'Encerramento' saying 'Obrigado por ouvir nosso programa!'
 }
